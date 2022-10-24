a76ee6a250b611a323846064cd92522c22fc9952
Jon Welling <jon@jonwelling.com>
2022-10-23 17:26:39 -0700

    Init Created script Added readme

---
+++ readme.md
@@ -0,0 +1,9 @@
+I use Code Server via docker. The image I used is based on linuxserver/code-server.
+
+It currently:
+- Updates image
+- Installs Node.js V16
+- Installs Reddis
+
+Set setup.sh to execute: chmod u+x setup.sh
+Run script: ./setup.sh
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+echo "####################"
+echo "Checking for and applying updates"
+echo "####################"
+sudo apt update
+sudo apt upgrade
+
+echo "####################"
+echo "Installing Node.js V16"
+echo "####################"
+cd ~
+curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt install nodejs
+
+echo "####################"
+echo "Installing Redis"
+echo "####################"
+sudo apt install redis-server
+sudo systemctl status redis
+
+echo "####################"
+echo "Done"
+echo "####################"
+echo " "
+echo " Node version: "
+node -v
+echo " Reddis version: "
+redis-server --version
