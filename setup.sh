6eb42ce09f812e665ed340c70776274d59b9ce78
Jon Welling <jon@jonwelling.com>
2022-10-28 16:22:39 -0700

    commit Maitenance

--- setup.sh
+++ setup.sh
@@ -14,6 +14,11 @@ sudo bash /tmp/nodesource_setup.sh
 sudo apt install nodejs
 
 echo "####################"
+echo "Installing Vue 3.X Latest"
+echo "####################"
+npm init vue@latest
+
+echo "####################"
 echo "Installing Redis"
 echo "####################"
 sudo apt install redis-server
